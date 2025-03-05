517430ddaddc37ab8be75fe0baa0494169e11c13
brando1607 <109045330+brando1607@users.noreply.github.com>
2025-03-04 23:56:26 -0300

    feat(booksRoutes): CRUD routes created for books

--- src/routes/books.routes.ts
+++ src/routes/books.routes.ts
@@ -1,3 +1,14 @@
 import { Router } from "express";
+import { BooksController } from "../controller/books.controller";
 
 export const router = Router();
+
+router.get("/getBooks", BooksController.getBooks);
+
+router.get("/getBook/:id", BooksController.getBook);
+
+router.post("/createBook", BooksController.createBook);
+
+router.put("/updateBook/:id", BooksController.updateBook);
+
+router.delete("/deleteBook/:id", BooksController.deleteBook);
